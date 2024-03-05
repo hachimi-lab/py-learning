d97fa2acc387dd8f21a029c429a958bfd1bd1c49
kaka <hachimi-lab@outlook.com>
2024-03-05 16:59:21 +0800

    005_set

---
+++ 005_set/set.py
@@ -0,0 +1,17 @@
+"""
+set is a collection of unique elements
+"""
+
+# 创建set
+s = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+print(type(s))
+
+# 增加元素
+s.add(11)
+print(s)
+
+# 删除元素
+s.remove(11)
+
+# 检查是否在集合中
+print(2 in s)
