53ca1bc9d9fa2d0dc9e446662ce71e6c525981ea
kaka <hachimi-lab@outlook.com>
2024-03-05 17:14:21 +0800

    007_module

---
+++ 007_module/module.py
@@ -0,0 +1,8 @@
+"""
+Module usage
+"""
+
+import mymodule
+
+mymodule.hello()
+mymodule.add(1, 2)
---
+++ 007_module/mymodule.py
@@ -0,0 +1,11 @@
+"""
+Module is a python file that contains python statements and definitions.
+"""
+
+
+def hello():
+    print('Hello, world!')
+
+
+def add(a: int, b: int) -> int:
+    return a + b
