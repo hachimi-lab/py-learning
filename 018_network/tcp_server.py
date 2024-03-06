eca690e93a9f139158a580bf80cf481408d6c32a
kaka <hachimi-lab@outlook.com>
2024-03-06 15:50:51 +0800

    018_network_tcp

--- 018_network/tcp_server.py
+++ 018_network/tcp_server.py
@@ -1,28 +1,77 @@
 import socket
 import threading
+import uuid
+from typing import Dict
 
 
-def handle_client(client_socket, addr):
-    print(f"接受来自 {addr} 的连接")
-    while True:
-        data = client_socket.recv(1024)
-        if not data:
-            break
-        print(f"收到来自 {addr} 的消息: {data.decode()}")
-        client_socket.send("收到你的消息".encode())
-    client_socket.close()
+class TcpSession:
+    def __init__(self, conn, addr, on_message, on_close):
+        self.conn = conn
+        self.addr = addr
+        self.on_message = on_message
+        self.on_close = on_close
+        self.thread = None
+        self.running = False
+
+    def start_read(self):
+        self.running = True
+        self.thread = threading.Thread(target=self.__read_from_conn)
+        self.thread.start()
+
+    def __read_from_conn(self):
+        try:
+            while self.running:
+                data = self.conn.recv(1024)
+                if not data:
+                    break
+                if self.on_message:
+                    self.on_message(self, data.decode())
+        except socket.error:
+            self.on_close()
+
+    def send_message(self, message):
+        self.conn.send(message.encode())
+
+    def close(self):
+        self.running = False
+        if self.thread:
+            self.thread.join()
+        self.conn.close()
 
 
-def start_server(ip, port):
-    server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    server.bind((ip, port))
-    server.listen(1)
-    print(f"服务器启动在 {ip}:{port}")
-    while True:
-        client_socket, addr = server.accept()
-        client_thread = threading.Thread(target=handle_client, args=(client_socket, addr))
-        client_thread.start()
+class TcpServer:
+    def __init__(self, ip, port):
+        self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sessions: Dict[uuid.UUID, TcpSession] = {}
+        self.socket.bind((ip, port))
+        print(f"服务器启动在 {ip}:{port}")
+
+    def start(self):
+        self.socket.listen(5)
+        while True:
+            conn, addr = self.socket.accept()
+            self.open_session(conn, addr)
+
+    def send_message(self, session_id: uuid.UUID, message):
+        if session_id in self.sessions:
+            self.sessions[session_id].send_message(message)
+
+    def open_session(self, conn, addr):
+        session = TcpSession(conn, addr, self.message_handle, self.close_session)
+        self.sessions[uuid.uuid4()] = session
+        session.start_read()
+
+    def close_session(self, session_id: uuid.UUID):
+        if session_id in self.sessions:
+            self.sessions[session_id].close()
+            del self.sessions[session_id]
+
+    @staticmethod
+    def message_handle(session: TcpSession, data):
+        print(f"收到来自 {session.addr} 的消息: {data}")
+        session.send_message("你好，客户端")
 
 
 if __name__ == '__main__':
-    start_server('127.0.0.1', 12345)
+    server = TcpServer('127.0.0.1', 12345)
+    server.start()
